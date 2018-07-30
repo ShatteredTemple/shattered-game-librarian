341658cbcc200148b1e5462fa8132182dcad26f4
Andrew Timson <atimson+github@gmail.com>
2018-07-29 21:25:20 -0400

    Initial solution template.

---
+++ src/ShatteredGameLibrarian/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ShatteredTemple.GameLibrarian
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
